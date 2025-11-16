93c9e3baff1b275757f3554e1c32b71d99f44f43
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-11-16 11:26:25 +0530

    feat: create home page layout with main components

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import BlogList from '../components/BlogList'
+import NewsLetter from '../components/newsLetter'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <BlogList />
+      <NewsLetter />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
