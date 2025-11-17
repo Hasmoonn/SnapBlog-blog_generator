dd8f70f569594297c7edd5123fc82ff88942290c
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-11-17 11:34:04 +0530

    add blog creation with ImageKit upload, comments API, publish toggle, and AI content generator

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,162 @@
+import imagekit from '../config/imageKit.js';
+import fs from 'fs'
+import Blog from '../models/Blog.js';
+import Comment from '../models/Comment.js';
+import main from '../config/gemini.js';
+
+export const addBlog = async (req, res) => {
+
+  try {
+    const {title, subTitle, description, category, isPublished} = JSON.parse(req.body.blog);
+
+    const imageFile = req.file;
+
+    // check if all fields are present 
+    if (!title || !description || !category || !imageFile) {
+      return res.json({success: false, message: "Missing required fields"})
+    }
+
+    const fileBuffer = fs.readFileSync(imageFile.path)
+
+    // upload image to imageKit 
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile,
+      folder: "/blogs"
+    })
+
+    // optimization through imagekit url transformation 
+    const optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        {quality: 'auto'}, // auto compression
+        {format: 'webp'}, // conver to modern format
+        {width: '1280'} // width resizing
+      ]
+    }) 
+
+    const image = optimizedImageUrl
+
+    await Blog.create({title, subTitle, description, category, image, isPublished})
+
+    res.json({success: true, message: "Blog added successfully"})
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+} 
+
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    
+    const blogs = await Blog.find({isPublished: true})
+
+    res.json({success: true, blogs})
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
+
+export const getBlogById = async (req, res) => {
+
+  try {
+
+    const {blogId} = req.params;
+
+    const blog = await Blog.findById(blogId)
+
+    if (!blog) {
+      return res.json({success: false, message: "Blog not found"})
+    }
+
+    res.json({success: true, blog})
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
+
+export const deleteBlogById = async (req, res) => {
+
+  try {
+
+    const {id} = req.body
+
+    await Blog.findByIdAndDelete(id)
+
+    await Comment.deleteMany({blog: id})
+
+    res.json({success: true, message: "Blog deleted successfully"})
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
+
+export const togglePublish = async (req, res) => {
+
+  try {
+    const {id} = req.body
+    
+    const blog = await Blog.findById(id)
+
+    blog.isPublished = !blog.isPublished;
+
+    await blog.save()
+
+    res.json({success: true, message: "Blog status updated"})
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
+
+// comments 
+export const addComment = async (req, res) => {
+
+  try {
+    const {blog, name, content} = req.body;
+
+    await Comment.create({blog, name, content});
+
+    res.json({success: true, message: "Comment added for review"})
+
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
+
+export const getBlogComments = async (req, res) => {
+
+  try {
+    const {blogId} = req.body;
+
+    const comments = await Comment.find({ blog: blogId, isApproved: true}).sort({createdAt: -1})
+
+    if (comments) {
+      res.json({success: true, comments})
+    } else {
+      return res.json({success: false, message: "No data there"})
+    }
+
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
+
+export const generateContent = async (req, res) => {
+  try {
+    const {prompt} = req.body;
+
+    const content = await main(`${prompt}. Generate a blog content for this topic in simple text format`)
+
+    res.json({success: true, content})
+    
+  } catch (error) {
+      res.json({success: false, message: error.message})
+  }
+}
+
